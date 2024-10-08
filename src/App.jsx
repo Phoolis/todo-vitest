58a8c4c3bee353c6495dd0246c62732d6d040ae0
Phoolis <carlsonps@gmail.com>
2024-10-08 19:25:29 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import './App.css'
+import TodoTable from './TodoTable';
+import { useState } from 'react';
+
+function App() {
+  const [todo, setTodo] = useState({ desc: "", date: "" });
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (event) => {
+    setTodos([...todos, todo]);
+    setTodo({ desc: "", date: "" });
+  };
+
+  const inputChanged = (event) => {
+    setTodo({ ...todo, [event.target.name]: event.target.value });
+  };
+
+  return (
+    <>
+      <h1>My Todolist</h1>
+      <div className="App">
+        <input
+          placeholder="Date"
+          name="date"
+          value={todo.date}
+          onChange={inputChanged}
+        />
+        <input
+          placeholder="Description"
+          name="desc"
+          value={todo.desc}
+          onChange={inputChanged}
+        />
+        <button onClick={addTodo}>Add</button>
+        <TodoTable todos={todos} />
+      </div>
+    </>
+  );
+}
+
+export default App
